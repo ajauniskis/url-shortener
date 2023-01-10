f834f644730897f1e1bd4bfc0dc889cf294e9f4a
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2023-01-11 00:57:36 +0200

    adding url repository

---
+++ app/repositories/__init__.py
@@ -0,0 +1,7 @@
+from app.repositories.abstract_repository import AbstractRepository
+from app.repositories.url_repository import UrlRepository
+
+__all__ = [
+    "AbstractRepository",
+    "UrlRepository",
+]
---
+++ app/repositories/url_repository.py
@@ -0,0 +1,10 @@
+from fastapi import Depends
+
+from app.infrastructure import AbstractDatabaseClient, get_database
+from app.repositories.abstract_repository import AbstractRepository
+
+
+class UrlRepository(AbstractRepository):
+    def __init__(self) -> None:
+        self.table_name = "url"
+        self.database: AbstractDatabaseClient = get_database(self.table_name)
