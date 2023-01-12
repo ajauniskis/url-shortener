85bdb3ce4a9d196b82e40ad7418859d30eddbb71
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2023-01-12 22:45:57 +0200

    changing to async test

--- tests/infrastructure/test_database.py
+++ tests/infrastructure/test_database.py
@@ -1,12 +1,12 @@
-from unittest import TestCase
+from unittest import IsolatedAsyncioTestCase
 from unittest.mock import patch
 
 from app.infrastructure import get_database
 
 
-class TestGetDatabase(TestCase):
+class TestGetDatabase(IsolatedAsyncioTestCase):
     @patch("app.infrastructure.config.DatabaseConfig")
-    def test_get_database__invalid_database_type__thorws(self, patch_config):
+    async def test_get_database__invalid_database_type__thorws(self, patch_config):
         patch_config.return_value.database_type = "invalid-database"
 
         with self.assertRaises(ValueError) as exception_context:
@@ -19,7 +19,7 @@ class TestGetDatabase(TestCase):
         )
 
     @patch("app.infrastructure.config.DatabaseConfig")
-    def test_get_database__returns_database_client(self, patch_config):
+    async def test_get_database__returns_database_client(self, patch_config):
         patch_config.return_value.database_type = "deta-base"
 
         database = get_database("some_table")
