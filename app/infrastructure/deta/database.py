fb1fd96611b651109ec7bb66d4b20ec2d65e6c3b
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2023-01-16 00:35:20 +0200

    adding create method

--- app/infrastructure/deta/database.py
+++ app/infrastructure/deta/database.py
@@ -1,3 +1,4 @@
+from app.domain import Url
 from app.infrastructure.abstract_database_client import AbstractDatabaseClient
 from app.infrastructure.deta.base import get_base
 
@@ -6,3 +7,7 @@ class DetaBaseClient(AbstractDatabaseClient):
     def __init__(self, table_name: str) -> None:
         super().__init__(table_name)
         self.base = get_base(self.table_name)
+
+    async def create(self, model):
+        response = await self.base.put([model.dict()])
+        return Url(**response["processed"]["items"][0])
