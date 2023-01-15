39ae71e6d551d1505f6bedf453c1166953a5facd
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2023-01-16 00:46:10 +0200

    adding logic to create_url endpoint

--- app/api/endpoints/url.py
+++ app/api/endpoints/url.py
@@ -1,16 +1,33 @@
+import secrets
+
 from fastapi import APIRouter
-from app.api.schemas import URLBase
+
+from app.api.schemas import CreateUrlRequest, CreateUrlResponse
+from app.domain import Url as UrlDomainModel
+from app.repositories import UrlRepository
 
 router = APIRouter(
     prefix="/url",
     tags=["url"],
 )
 
+url_repository = UrlRepository()
+
 
 @router.post(
     "/",
     summary="Create short URL",
+    response_model=CreateUrlResponse,
 )
-def create_url(url: URLBase):
+async def create_url(url: CreateUrlRequest):
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    secret_key = "".join(secrets.choice(chars) for _ in range(8))
 
-    return "Not implemented yet"
+    url_domain = UrlDomainModel(
+        secret_key=secret_key,
+        target_url=url.target_url,
+    )
+
+    response = await url_repository.create_url(url_model=url_domain)
+
+    return response
