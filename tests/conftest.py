c5c41cc350b2b00c13acf37b5546893c3cecf479
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2023-01-17 22:12:11 +0200

    adding test domain model

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,12 @@
 import os
+from typing import Optional
+
+from pydantic import BaseModel
 
 os.environ["USE_CACHED_SETTINGS"] = "FALSE"
 os.environ["ENV"] = "test"
+
+
+class TestDomainModel(BaseModel):
+    key: Optional[str] = None
+    value: str
