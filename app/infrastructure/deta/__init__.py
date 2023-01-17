20c3f242b341406ada59ee3db4c355f1d6729034
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2023-01-18 01:26:04 +0200

    including get_deta_base_config

--- app/infrastructure/deta/__init__.py
+++ app/infrastructure/deta/__init__.py
@@ -1,5 +1,7 @@
+from app.infrastructure.deta.config import get_deta_base_config
 from app.infrastructure.deta.database import DetaBaseClient
 
 __all__ = [
     "DetaBaseClient",
+    "get_deta_base_config",
 ]
