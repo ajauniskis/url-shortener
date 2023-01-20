a9f2a80af3bf1a33f2bf00db07c175c2f85b0211
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2023-01-20 23:33:17 +0200

    adding get method

--- app/infrastructure/deta/database.py
+++ app/infrastructure/deta/database.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 from app.infrastructure.abstract_database_client import AbstractDatabaseClient
 from app.infrastructure.deta.base import get_base
 
@@ -10,3 +12,7 @@ class DetaBaseClient(AbstractDatabaseClient):
     async def create(self, model):
         response = await self.base.put([model.dict()])
         return model.parse_obj(response["processed"]["items"][0])
+
+    async def get(self, key: str) -> Dict[str, Any]:
+        response = await self.base.get(key)
+        return response
