1db17241e9b640f2d89fcb439137617a1f7792a2
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2023-01-25 01:36:50 +0200

    adding query method

--- app/infrastructure/abstract_database_client.py
+++ app/infrastructure/abstract_database_client.py
@@ -1,13 +1,19 @@
-from abc import ABC
-from typing import Any, Dict
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List
 
 
 class AbstractDatabaseClient(ABC):
     def __init__(self, table_name: str) -> None:
         self.table_name = table_name
 
+    @abstractmethod
     async def create(self, model):
         raise NotImplementedError
 
+    @abstractmethod
     async def get(self, key: str) -> Dict[str, Any]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def query(self, query: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
         raise NotImplementedError
