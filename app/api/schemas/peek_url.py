51942213b0d236655df95d7df63df59e4394b879
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2023-02-01 22:19:48 +0200

    adding schema example

--- app/api/schemas/peek_url.py
+++ app/api/schemas/peek_url.py
@@ -2,4 +2,7 @@ from app.api.schemas.url import URLBase
 
 
 class PeekUrlResponse(URLBase):
-    pass
+    class Config:
+        schema_extra = {
+            "example": {"target_url": "https://google.com"},
+        }
