643bb0d7bd69a830327fced97010d62ba56693d3
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2023-02-03 01:23:13 +0200

    registering qr routes

--- app/api/endpoints/router.py
+++ app/api/endpoints/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import url
+from app.api.endpoints import qr
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(url.router)
+api_router.include_router(qr.router)
