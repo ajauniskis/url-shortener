8cc36b0443c69cc1a664111567b83959ba4ef720
Algirdas Jauniskis <algirdas.jauniskis@revelsystems.com>
2023-02-03 09:03:34 +0200

    sorting imports

--- app/api/endpoints/router.py
+++ app/api/endpoints/router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import url
-from app.api.endpoints import qr
+from app.api.endpoints import qr, url
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(url.router)
--- app/api/schemas/qr.py
+++ app/api/schemas/qr.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+
 from app.domain import QR_CODE_RENDER_STYLES
 
 
